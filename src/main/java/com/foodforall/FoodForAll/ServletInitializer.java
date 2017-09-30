492e26bafdfa3be753236ca6b2e1ab8fae3cce98
fshaik <fshaik@192.168.0.12>
2017-09-30 13:33:55 -0600

    Initial Project Setup

---
+++ src/main/java/com/foodforall/FoodForAll/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.foodforall.FoodForAll;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FoodForAllApplication.class);
+	}
+
+}
---
+++ src/test/java/com/foodforall/FoodForAll/FoodForAllApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foodforall.FoodForAll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodForAllApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
